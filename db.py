2f2dbd7b027b4ba87406b472e62520b96a3b01c3
Basil <basil@summation.app>
2020-12-28 19:10:12 -0800

    logger adapter

--- db.py
+++ db.py
@@ -19,7 +19,7 @@ from sqlalchemy.orm import sessionmaker, scoped_session
 from sqlalchemy import text
 from sqlalchemy.ext.declarative import declarative_base
 
-logger = logging.getLogger(__name__)
+logger = logging.LoggerAdapter(logging.getLogger(__name__), {'organization_id': 0})
 
 ADMIN_PASSWORD = os.getenv('ADMIN_PASSWORD')
 ENVIRONMENT = os.getenv('ENVIRONMENT')
