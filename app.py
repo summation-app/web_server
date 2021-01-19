e0dcef7e5ef199b66062d4f99643233789e3cd7a
Basil <basil@summation.app>
2021-01-18 18:56:24 -0800

    only save enabled databases apis if not null

--- app.py
+++ app.py
@@ -435,10 +435,10 @@ AND t3.value->>'scope'='production'"""
 				if record:
 					record.name = data.get('name')
 					await record.save()
-					enabled_databases = data.get('enabled_databases')
-					enabled_apis = data.get('enabled_apis')
-					settings_record, created = await get_or_create(0, 'summation', Settings, organization_id=organization_id, application_id=id, key='enabled_databases', value=enabled_databases)
-					settings_record, created = await get_or_create(0, 'summation', Settings, organization_id=organization_id, application_id=id, key='enabled_apis', value=enabled_apis)
+					if enabled_databases := data.get('enabled_databases'):
+						settings_record, created = await get_or_create(0, 'summation', Settings, organization_id=organization_id, application_id=id, key='enabled_databases', value=enabled_databases)
+					if enabled_apis := data.get('enabled_apis'):
+						settings_record, created = await get_or_create(0, 'summation', Settings, organization_id=organization_id, application_id=id, key='enabled_apis', value=enabled_apis)
 					return JSONResponse(True, status_code=200)
 				else:
 					logger.error('could not find record to enable/disable')
