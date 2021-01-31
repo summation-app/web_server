fbeddd9c687d23524d0d11654847419b90f3b7c9
Basil <basil@summation.app>
2021-01-30 17:48:13 -0800

    debugging

--- secrets_manager.py
+++ secrets_manager.py
@@ -35,6 +35,7 @@ class SecretsManager():
 		if result.application_id:
 			self.connections_for_orgs_apps[result.organization_id][result.application_id] = Secrets.create_manager(result.value.get('protocol'), **result.value)
 		elif result.organization_id:
+			logger.debug("creating secrets connection for org_id: {result.organization_id}")
 			self.connections_for_orgs[result.organization_id] = Secrets.create_manager(result.value.get('protocol'), **result.value)
 
 	def get_manager(self, organization_id, application_id=None):
